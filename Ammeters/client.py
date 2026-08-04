9c81e4cd34cd245d4cad4c95d5e91a591c78b710
hlreina <79912669+BakerMalks@users.noreply.github.com>
2026-08-04 11:33:05 +0300

    change request_current_from_ammeter to use ammeter instance

--- Ammeters/client.py
+++ Ammeters/client.py
@@ -1,13 +1,14 @@
 from socket import socket, AF_INET, SOCK_STREAM
+from Ammeters.base_ammeter import AmmeterEmulatorBase
 
 
-def request_current_from_ammeter(port: int, command: bytes):
+def request_current_from_ammeter(ammeter: AmmeterEmulatorBase, command: bytes):
     with socket(AF_INET, SOCK_STREAM) as s:
-        s.connect(('localhost', port))
+        s.connect(('localhost', ammeter.port))
         s.sendall(command)
         data = s.recv(1024)
         if data:
-            print(f"Received current measurement from port {port}: {data.decode('utf-8')} A")
+            print(f"Received current measurement from port {ammeter.port}: {data.decode('utf-8')} A")
         else:
             print("No data received.")
 
