774066108b62bdc87dc41edf7d6f988d2c7c2833
hlreina <79912669+BakerMalks@users.noreply.github.com>
2026-08-04 12:20:59 +0300

    main use the config file

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import threading
 import time
 
+
+from src.utils.config import load_config
 from Ammeters.base_ammeter import AmmeterEmulatorBase
 from Ammeters.Circutor_Ammeter import CircutorAmmeter
 from Ammeters.Entes_Ammeter import EntesAmmeter
@@ -14,18 +16,16 @@ def run_emulator(emulator: AmmeterEmulatorBase):
 
 if __name__ == "__main__":
     # Start each ammeter in a separate thread
-    greenlee = GreenleeAmmeter(5001)
-    entes = EntesAmmeter(5002)
-    circutor = CircutorAmmeter(5003)
+    config = load_config("./config/config.yaml")
+    greenlee = GreenleeAmmeter(config["ammeters"]["greenlee"]["port"])
+    entes = EntesAmmeter(config["ammeters"]["entes"]["port"])
+    circutor = CircutorAmmeter(config["ammeters"]["circutor"]["port"])
     ammeters = [greenlee, entes, circutor]
     for ammeter in ammeters:
         threading.Thread(target=run_emulator, args=[ammeter], daemon=True).start()
 
-    # This section is commented out because it shouldn't work.
-    # Read the README.md file as well as the source code if you need, and fix the problem.
-
-    # Wait for the servers to start, if you have problem restarting the servers between runs try increasing sleep time.
     time.sleep(5)
+    
     request_current_from_ammeter(greenlee, greenlee.get_current_command)  # Request from Greenlee Ammeter
     request_current_from_ammeter(entes, entes.get_current_command)  # Request from ENTES Ammeter
     request_current_from_ammeter(circutor, circutor.get_current_command)  # Request from CIRCUTOR Ammeter
