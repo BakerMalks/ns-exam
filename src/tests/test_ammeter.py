f4bbd2e121d3c179e5ee868c9946583a6769cb5b
hlreina <79912669+BakerMalks@users.noreply.github.com>
2026-08-09 13:46:26 +0300

    changed to sampling_kwargs and added std cutoff

--- src/tests/test_ammeter.py
+++ src/tests/test_ammeter.py
@@ -1,7 +1,7 @@
 import pytest
 import logging
 
-from typing import Callable
+from typing import Callable, Dict, Optional
 
 from Ammeters.base_ammeter import AmmeterEmulatorBase
 from src.utils.result_manager import ResultManager, Sampler, NumericSamplerResult
@@ -10,14 +10,20 @@ from Ammeters.Entes_Ammeter import EntesAmmeter
 from Ammeters.Greenlee_Ammeter import GreenleeAmmeter
 
 
+STD_CUTOFF = 10
+
 @pytest.mark.stability
 @pytest.mark.circutor
 @pytest.mark.parametrize("time_to_run", [10, 30])
 @pytest.mark.parametrize("ammeter", [CircutorAmmeter], indirect=True)
 def test_circutor_stability(result_manager, make_sampler, time_to_run, ammeter):
-    _test_single_ammeter_stability(file_name="ammeter_result", result_manager=result_manager,
-                                   make_sampler=make_sampler, time_to_run=time_to_run,
-                                   ammeter=ammeter)
+    sampling_kwargs = {"total_duration_seconds": time_to_run}
+    res = _test_single_ammeter_stability(file_name="ammeter_result",
+                                         result_manager=result_manager,
+                                         make_sampler=make_sampler,
+                                         sampling_kwargs=sampling_kwargs,
+                                         ammeter=ammeter)
+    assert res.std() <= STD_CUTOFF, "Standard deviation is to large for stable result"
 
 
 @pytest.mark.stability
@@ -25,9 +31,13 @@ def test_circutor_stability(result_manager, make_sampler, time_to_run, ammeter):
 @pytest.mark.parametrize("time_to_run", [10, 30])
 @pytest.mark.parametrize("ammeter", [EntesAmmeter], indirect=True)
 def test_entes_stability(result_manager, make_sampler, time_to_run, ammeter):
-    _test_single_ammeter_stability(file_name="ammeter_result", result_manager=result_manager,
-                                   make_sampler=make_sampler, time_to_run=time_to_run,
-                                   ammeter=ammeter)
+    sampling_kwargs = {"total_duration_seconds": time_to_run}
+    res = _test_single_ammeter_stability(file_name="ammeter_result",
+                                         result_manager=result_manager,
+                                         make_sampler=make_sampler,
+                                         sampling_kwargs=sampling_kwargs,
+                                         ammeter=ammeter)
+    assert res.std() <= STD_CUTOFF, "Standard deviation is to large for stable result"
 
 
 @pytest.mark.stability
@@ -35,9 +45,13 @@ def test_entes_stability(result_manager, make_sampler, time_to_run, ammeter):
 @pytest.mark.parametrize("time_to_run", [10, 30])
 @pytest.mark.parametrize("ammeter", [GreenleeAmmeter], indirect=True)
 def test_greenlee_stability(result_manager, make_sampler, time_to_run, ammeter):
-    _test_single_ammeter_stability(file_name="ammeter_result", result_manager=result_manager,
-                                   make_sampler=make_sampler, time_to_run=time_to_run,
-                                   ammeter=ammeter)
+    sampling_kwargs = {"total_duration_seconds": time_to_run}
+    res = _test_single_ammeter_stability(file_name="ammeter_result",
+                                         result_manager=result_manager,
+                                         make_sampler=make_sampler,
+                                         sampling_kwargs=sampling_kwargs,
+                                         ammeter=ammeter)
+    assert res.std() <= STD_CUTOFF, "Standard deviation is to large for stable result"
 
 
 @pytest.mark.stability
@@ -45,9 +59,35 @@ def test_greenlee_stability(result_manager, make_sampler, time_to_run, ammeter):
 @pytest.mark.parametrize("sampling_frequency_hz", [1, 30])
 @pytest.mark.parametrize("ammeter", [CircutorAmmeter, EntesAmmeter, GreenleeAmmeter], indirect=True)
 def test_ammeter_stability(result_manager, make_sampler, time_to_run, ammeter, sampling_frequency_hz):
-    res = _test_single_ammeter_stability(file_name="ammeter_result", result_manager=result_manager,
-                                         make_sampler=make_sampler, time_to_run=time_to_run,
-                                         ammeter=ammeter, sampling_frequency_hz=sampling_frequency_hz)
+    sampling_kwargs = {"total_duration_seconds": time_to_run,
+                       "sampling_frequency_hz": sampling_frequency_hz}
+    res = _test_single_ammeter_stability(file_name="ammeter_result",
+                                         result_manager=result_manager,
+                                         make_sampler=make_sampler,
+                                         ammeter=ammeter,
+                                         sampling_kwargs=sampling_kwargs)
+    result_manager.plot_results
+    # Plot
+    title = ammeter.name.capitalize()
+    value_label = "Current[A]"
+    result_manager.plot_results(res, title=title, kind="scatter", value_label=value_label)
+    result_manager.plot_results(res, title=title, kind="line", value_label=value_label)
+    result_manager.plot_results(res, title=title, kind="hist", value_label=value_label)
+    assert res.std() <= STD_CUTOFF, "Standard deviation is to large for stable result"
+
+
+@pytest.mark.stability
+@pytest.mark.parametrize("measurements_count", [100])
+@pytest.mark.parametrize("sampling_frequency_hz", [1, 20])
+@pytest.mark.parametrize("ammeter", [CircutorAmmeter, EntesAmmeter, GreenleeAmmeter], indirect=True)
+def test_ammeter_stability_by_measurements_count(result_manager, make_sampler, time_to_run, ammeter, sampling_frequency_hz):
+    sampling_kwargs = {"total_duration_seconds": time_to_run,
+                       "sampling_frequency_hz": sampling_frequency_hz}
+    res = _test_single_ammeter_stability(file_name="ammeter_result",
+                                         result_manager=result_manager,
+                                         make_sampler=make_sampler,
+                                         ammeter=ammeter,
+                                         sampling_kwargs=sampling_kwargs)
     result_manager.plot_results
     # Plot
     title = ammeter.name.capitalize()
@@ -93,10 +133,12 @@ def test_ammeters_at_same_time_stability(result_manager, make_sampler, time_to_r
     result_manager.plot_results(*results.values(), title=title, kind="hist", value_label=value_label)
 
 
-def _test_single_ammeter_stability(file_name: str, result_manager: ResultManager, make_sampler: Callable[..., Sampler],
-                                   time_to_run: int, ammeter: AmmeterEmulatorBase,
-                                   sampling_frequency_hz: int = 1) -> NumericSamplerResult:
-    sampler = make_sampler(total_duration_seconds=time_to_run, sampling_frequency_hz=sampling_frequency_hz)
+def _test_single_ammeter_stability(file_name: str, result_manager: ResultManager,
+                                   make_sampler: Callable[..., Sampler],
+                                   ammeter: AmmeterEmulatorBase,
+                                   sampling_kwargs: Optional[Dict[str, float]] = None) -> NumericSamplerResult:
+    sampling_kwargs = sampling_kwargs if sampling_kwargs else {}
+    sampler = make_sampler(**sampling_kwargs)
     logging.info(sampler)
     res: NumericSamplerResult = sampler.sample(ammeter.measure_current, result_class=NumericSamplerResult)
     logging.info(res)
